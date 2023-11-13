2aaed89632cabad80ecfdb321e86d450d2183ba4
wawatusee <web2020.kieran.labarrere@gmail.com>
2023-11-13 18:15:14 +0100

    correction index.php public and root

--- index.php
+++ index.php
@@ -1,13 +1,1 @@
-<?php
-require_once("../src/model/config_model.php");
-require_once("../src/model/menus_model.php");
-require_once "../config/config.php";?>
-<!DOCTYPE html>
-<html lang=<?=$lang?> prefix="og:http://ogp.me/ns#">
-    <?php require_once "../inc/head.php"?>
-    <body>
-        <?php require_once "../inc/header.php"?>
-        <?php require_once "../inc/main.php"?>
-        <?php require_once "../inc/footer.php"?>
-    </body> 
-</html>
+<?php header('Location:/public/index.php');
