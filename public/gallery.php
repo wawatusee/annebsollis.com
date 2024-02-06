554e0c71382234d138155205e3c818a0e160e2fb
wawatusee <web2020.kieran.labarrere@gmail.com>
2024-02-06 16:51:02 +0100

    fin structure, style.css, mise en place galeries et gallery.php

---
+++ public/gallery.php
@@ -0,0 +1,10 @@
+<?php
+    require_once "../inc/header.php"
+?>
+<section class="gallery">
+    <h2>Gallery</h2>
+    <?php
+    $galleryInfo=$_GET;
+    var_dump($galleryInfo);
+    ?>
+</section>
---
+++ src/view/readme.example.php
@@ -0,0 +1,3 @@
+<section class="readme">
+
+</section>
